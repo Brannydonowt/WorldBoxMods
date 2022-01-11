f88898bb0d6868ba2a74ff8113c9bbe600d7d031
Branny <brandonsidebottom@outlook.com>
2022-01-11 14:48:27 +0000

    Top Killer tracker

--- BrannyTestMods/Mod/WorldBoxMod.MoreStats.Tracker.cs
+++ BrannyTestMods/Mod/WorldBoxMod.MoreStats.Tracker.cs
@@ -14,6 +14,9 @@ namespace BrannyTestMods
 {
     public partial class WorldBoxMod
     {
+		public static ActorStatus killLeader;
+		public static int highestKills;
+
 		public static void stats_patch(Harmony harmony) 
 		{
 			Helper.Utils.HarmonyPatching(harmony, "postfix", AccessTools.Method(typeof(Actor), "increaseKillCount"), AccessTools.Method(typeof(WorldBoxMod), "increaseKillCount_postfix"));
@@ -22,10 +25,32 @@ namespace BrannyTestMods
 
 		public static void increaseKillCount_postfix(Actor __instance)
 		{
-			var kills = Reflection.GetField(__instance.GetType(), __instance, "kills") as string;
-			Debug.Log(__instance.name + " Kills:- " + kills);
-			//int kills = ((ActorBase)__instance).kills;
-			//Debug.Log("Added Trait: " + newTrait.id);
+			var data = (ActorStatus)Reflection.GetField(__instance.GetType(), __instance, "data");
+			int kills = data.kills;
+
+			if (CompareKillLeader(data, kills)) 
+			{
+				Debug.Log("We have a new kill leader!");
+				Debug.Log(data.firstName + " : " + kills + " kills");
+			}
+		}
+
+		public static bool CompareKillLeader(ActorStatus killer, int numKills) 
+		{
+			if (numKills > highestKills)
+			{
+				killLeader = killer;
+				highestKills = numKills;
+				return true;
+			}
+			else
+			if (numKills == highestKills) 
+			{
+				Debug.Log("We have a contender for the most ruthless!");
+				Debug.Log(killer.firstName + " has reached " + highestKills + " total kills!");
+			}
+
+			return false;
 		}
 	}
 }
